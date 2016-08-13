e91c9a0145e13a77b858a21797b064a390e32cd6
JoshuaTang <1240604020@qq.com>
2016-08-13 06:59:08 -0700

    add youtube search

--- src/index.js
+++ src/index.js
@@ -18,11 +18,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 
 const API_KEY = "AIzaSyARzLoBx5a9YOTHmWUFGxIyzDLpnUxYcO4";
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
 
 // Create a new component. This component should produce some HTML.
 // const: final value of the variable.
