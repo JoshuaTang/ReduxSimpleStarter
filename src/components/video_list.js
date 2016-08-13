6ddd758a98fefb61f2350631bb7b337ce3a8c231
JoshuaTang <1240604020@qq.com>
2016-08-13 11:25:28 -0700

    add video list item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import VideoListItem from './video_list_item'
+
 
 // I need to pass videos from parent component to
 // VideoList.
 const VideoList = (props) => {
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+
     </ul>
   );
 };
