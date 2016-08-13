d267f14226108831126f8464f5430362dcf269c2
JoshuaTang <1240604020@qq.com>
2016-08-13 06:26:13 -0700

    add arrow function

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,12 +6,9 @@ import React, {Component} from 'react';
 // React.Component could be simplified to Component.
 class SearchBar extends Component {
   render() {
-    return <input onChange={this.onInputChange} />;
+    return <input onChange={event => console.log(event.target.value)} />;
   }
 
-  onInputChange(event) {
-    console.log(event.target.value);
-  }
 }
 
 export default SearchBar;
