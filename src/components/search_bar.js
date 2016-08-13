035cd8a8a287e752df75a3ed276c5c438f4f82bd
JoshuaTang <1240604020@qq.com>
2016-08-13 06:36:42 -0700

    add state of search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,11 +2,25 @@ import React, {Component} from 'react';
 
 
 // class-based component vs. function-based component
+// only class-based component has constructor.
+// state is a javascript object, exists in class-based component.
+
 
 // React.Component could be simplified to Component.
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    return (
+      <div>
+        <input onChange={event => this.setState({term: event.target.value})} />
+        Value of the input: {this.state.term}
+      </div>
+    )
   }
 
 }
